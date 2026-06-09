6edec362eca41b6682fd468946ed011e88e6abb6
darrell.parkhouse.5150 <darrellparkhouse82@gmail.com>
2026-06-09 06:27:51 -0500

    Update auth routes

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import * as authController from '../controllers/authController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 export const authRoutes = Router();
 
 authRoutes.post('/register', asyncHandler(authController.register));
 authRoutes.post('/login', asyncHandler(authController.login));
+authRoutes.get('/me', authMiddleware, asyncHandler(authController.me));
